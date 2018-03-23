dc6fd5d4852d242df789308ad453c205777e5e6f
Pierre Lambo <pierrelambo@googlemail.com>
2018-03-23 14:07:08 +0100

    jpeg extension added for pictures

--- src/main/java/org/npn/ContactManagementApplication.java
+++ src/main/java/org/npn/ContactManagementApplication.java
@@ -22,14 +22,14 @@ public class ContactManagementApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		/*DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		/*
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 				
-		contactRepository.save((new Contact("Tom","Kemson",df.parse("21/12/1955"),"tom@yahoo.fr",25234523L,"picture1")));
-		contactRepository.save((new Contact("Alain","Ballon",df.parse("22/11/1978"),"alain@yahoo.fr",15234523L,"picture2")));
-		contactRepository.save((new Contact("Paul","Fenton",df.parse("23/10/1968"),"paul@yahoo.fr",35234523L,"picture3")));
-		contactRepository.save((new Contact("Jouni","Moustapha",df.parse("24/05/1998"),"jouni@yahoo.fr",45234523L,"picture4")));
-		contactRepository.save((new Contact("Helene","Tomson",df.parse("25/04/1998"),"helene@yahoo.fr",55234523L,"picture5")));
+		contactRepository.save((new Contact("Tom","Kemson",df.parse("21/12/1955"),"tom@yahoo.fr",25234523L,"picture1.jpeg")));
+		contactRepository.save((new Contact("Alain","Ballon",df.parse("22/11/1978"),"alain@yahoo.fr",15234523L,"picture2.jpeg")));
+		contactRepository.save((new Contact("Paul","Fenton",df.parse("23/10/1968"),"paul@yahoo.fr",35234523L,"picture3.jpeg")));
+		contactRepository.save((new Contact("Jouni","Moustapha",df.parse("24/05/1998"),"jouni@yahoo.fr",45234523L,"picture.jpeg")));
+		contactRepository.save((new Contact("Helene","Tomson",df.parse("25/04/1998"),"helene@yahoo.fr",55234523L,"picture5.jpeg")));
 			
 		contactRepository.findAll().forEach(x-> System.out.println(x.getLastName()));
 */
